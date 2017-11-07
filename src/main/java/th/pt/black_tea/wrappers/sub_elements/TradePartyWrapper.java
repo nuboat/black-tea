7bf32e49544e1459bbe738481fb433731a13f0e8
passapong.t <passapong.t@gmail.com>
2017-11-07 15:05:52 +0700

    created and added getInstance, private constructor, getElement, parseTaxId

---
+++ src/main/java/th/pt/black_tea/wrappers/sub_elements/TradePartyWrapper.java
@@ -0,0 +1,36 @@
+package th.pt.black_tea.wrappers.sub_elements;
+
+import etda.uncefact.data.standard.taxinvoice_reusableaggregatebusinessinformationentity._2.TaxRegistrationType;
+import etda.uncefact.data.standard.taxinvoice_reusableaggregatebusinessinformationentity._2.TradePartyType;
+import th.pt.black_tea.wrappers.fields.Max256Wrapper;
+import th.pt.black_tea.wrappers.fields.Max35Wrapper;
+
+public class TradePartyWrapper {
+
+    private static TradePartyWrapper wrapper = null;
+
+    private TradePartyWrapper() {
+
+    }
+
+    public static TradePartyWrapper getInstance() {
+        if (wrapper == null)
+            wrapper = new TradePartyWrapper();
+        return wrapper;
+    }
+
+    public TradePartyType getElement(String name, String taxId) {
+        TradePartyType tradeParty = new TradePartyType();
+        tradeParty.setName(Max256Wrapper.text(name));
+        tradeParty.setSpecifiedTaxRegistration(parseTaxId(taxId));
+        //TODO add tradeParty.setPostalTradeAddress();
+        return tradeParty;
+    }
+
+    private TaxRegistrationType parseTaxId(String taxId) {
+        TaxRegistrationType taxRegistration = new TaxRegistrationType();
+        taxRegistration.setID(Max35Wrapper.id(taxId, "TXID"));
+
+        return taxRegistration;
+    }
+}
