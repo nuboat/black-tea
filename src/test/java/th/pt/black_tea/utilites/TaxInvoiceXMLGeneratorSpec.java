9445442b4d9ded9fda8b83af65ad5b3a1f31c2e1
passapong.t <passapong.t@gmail.com>
2017-11-08 14:47:23 +0700

    added SupplyChainTradeTransaction on canGenerateFullXML

--- src/test/java/th/pt/black_tea/utilites/TaxInvoiceXMLGeneratorSpec.java
+++ src/test/java/th/pt/black_tea/utilites/TaxInvoiceXMLGeneratorSpec.java
@@ -1,10 +1,16 @@
 package th.pt.black_tea.utilites;
 
 import etda.uncefact.data.standard.taxinvoice_crossindustryinvoice._2.TaxInvoiceCrossIndustryInvoiceType;
+import etda.uncefact.data.standard.taxinvoice_reusableaggregatebusinessinformationentity._2.HeaderTradeAgreementType;
+import etda.uncefact.data.standard.taxinvoice_reusableaggregatebusinessinformationentity._2.SupplyChainTradeTransactionType;
+import etda.uncefact.data.standard.taxinvoice_reusableaggregatebusinessinformationentity._2.TradePartyType;
 import org.junit.Test;
+import th.pt.black_tea.mocks.TradeAddressMock;
 import th.pt.black_tea.utilities.TaxInvoiceXMLGenerator;
 import th.pt.black_tea.wrappers.main_elements.ExchangedDocumentContextWrapper;
 import th.pt.black_tea.wrappers.main_elements.ExchangedDocumentWrapper;
+import th.pt.black_tea.wrappers.main_elements.SupplyChainTradeTransactionWrapper;
+import th.pt.black_tea.wrappers.sub_elements.TradePartyWrapper;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Date;
@@ -27,8 +33,23 @@ public class TaxInvoiceXMLGeneratorSpec {
         TaxInvoiceXMLGenerator generator = TaxInvoiceXMLGenerator.getInstance();
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         TaxInvoiceCrossIndustryInvoiceType taxInvoice = new TaxInvoiceCrossIndustryInvoiceType();
+
         ExchangedDocumentContextWrapper docContext = ExchangedDocumentContextWrapper.getInstance();
         ExchangedDocumentWrapper wrapper = ExchangedDocumentWrapper.getInstance();
+
+        SupplyChainTradeTransactionWrapper supWrapper = SupplyChainTradeTransactionWrapper.getInstance();
+        SupplyChainTradeTransactionType chain = supWrapper.getDefault();
+
+        HeaderTradeAgreementType agreement = new HeaderTradeAgreementType();
+        TradePartyWrapper tradeWrapper = TradePartyWrapper.getInstance();
+        TradePartyType buyer = tradeWrapper.getElement("Buyer Corp", "24524r45434", TradeAddressMock.mockBuyerAddress());
+        TradePartyType seller = tradeWrapper.getElement("Seller Corp", "467456735473", TradeAddressMock.mockSellerAddress());
+
+        agreement.setSellerTradeParty(seller);
+        agreement.setBuyerTradeParty(buyer);
+        chain.setApplicableHeaderTradeAgreement(agreement);
+
+        taxInvoice.setSupplyChainTradeTransaction(chain);
         taxInvoice.setExchangedDocument(wrapper.getTaxInvoice("TIV-00125", new Date()));
         taxInvoice.setExchangedDocumentContext(docContext.getDefault());
 
