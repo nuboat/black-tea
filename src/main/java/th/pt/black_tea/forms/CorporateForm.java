4441c0cef073ec57c1a52ec76f0fe6de30e487ee
passapong.t <passapong.t@gmail.com>
2017-11-08 17:19:55 +0700

    created and added public constructor

---
+++ src/main/java/th/pt/black_tea/forms/CorporateForm.java
@@ -0,0 +1,14 @@
+package th.pt.black_tea.forms;
+
+public class CorporateForm {
+
+    public CorporateForm(String name, String taxId) {
+        this.name = name;
+        this.taxId = taxId;
+    }
+
+    public String name;
+
+    public String taxId;
+
+}
